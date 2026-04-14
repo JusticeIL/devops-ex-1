bc47a923eff2a8472cb924489ae8189f21ef7695
JusticeIL <galru3@mta.ac.il>
2026-04-14 11:44:42 +0300

    changed Gal.java

--- Gal.java
+++ Gal.java
@@ -2,7 +2,7 @@ public class Gal {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Gal");
+            System.out.println("Gal is thanking Moshe for the last lecture");
         }
     }
 }
